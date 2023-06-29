de3ff106554a2a364a0874eb17f67483a829a937
Dustin Johns <dustin.johns1@gmail.com>
2023-06-28 23:25:52 -0500

    finished password generator

--- Password Generator/Password Generator.py
+++ Password Generator/Password Generator.py
@@ -2,18 +2,49 @@
 
 import random
 
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+MIN_CHARS = 8
+MAX_CHARS = 16
+
+# create a list of each type of character in a typical password, along with a union of the whole group.
+lc_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+uc_letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+all_chars = uc_letters + lc_letters + numbers + symbols
 
 print("*** PASSWORD GENERATOR ***")
-nr_letters= int(input("How many letters would you like in your password?\n")) 
-nr_symbols = int(input(f"How many symbols would you like?\n"))
-nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-#Eazy Level - Order not randomised:
-#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+# limit the user's input, but give them a significant range for password length.
+nr_chars = int(input(f"How many characters would you like in your password?({MIN_CHARS}-{MAX_CHARS})\n"))
 
+# enforce that we are only allowing the 
+while nr_chars < MIN_CHARS or nr_chars > MAX_CHARS:
+    if nr_chars < MIN_CHARS:
+        print(f"error: {nr_chars} is not enough characters for a secure password.")
+    elif nr_chars > MAX_CHARS:
+        print(f"error: {nr_chars} is too many characters for this password generator.")
 
-#Hard Level - Order of characters randomised:
-#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+    nr_chars = int(input("How many characters would you like in your password?(8-16)\n"))
+
+#hold the password in a list temporarily to make the manipulation simple
+password_list = []
+
+#get 1 of each of our types of characters and add to the password
+password_list.append(random.choice(lc_letters))
+password_list.append(random.choice(uc_letters))
+password_list.append(random.choice(numbers))
+password_list.append(random.choice(symbols))
+
+# add the rest of the password's characters randomly from all character options
+for char in range(1, nr_chars + 1):
+    password_list.append(random.choice(all_chars))
+
+# randomize the characters in the list
+random.shuffle(password_list)
+
+# convert the list to a string and present it to the user.
+password = ''
+for char in password_list:
+    password += char
+
+print(f"Your password is: {password}")
