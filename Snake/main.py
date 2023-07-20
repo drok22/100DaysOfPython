b438393445445ccd38278d3f2e601a1e2587471a
Dustin Johns <dustin.johns1@gmail.com>
2023-07-20 07:01:42 -0500

    making snake tail collision checking more efficient

--- Snake/main.py
+++ Snake/main.py
@@ -41,8 +41,8 @@ while game_running:
         game_running = False
         scoreboard.game_over()
     
-    for segment in snake.segments:
-        if segment != snake.head and snake.head.distance(segment) < 10:
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
             game_running = False
             scoreboard.game_over()
 
--- Turtle Graphics/main.py
+++ Turtle Graphics/main.py
@@ -2,6 +2,10 @@ import random
 import turtle as turtle_module
 
 tim = turtle_module.Turtle()
+screen = turtle_module.Screen()
+screen.bgcolor("black")
+
+'''
 turtle_module.colormode(255)
 
 def random_color():
@@ -11,8 +15,6 @@ def random_color():
     color = (r, g, b)
     return color
 
-########### Challenge 5 - Spirograph ########
-
 def draw_spirograph(size_of_gap):
     for _ in range(int(360 / size_of_gap)):
         tim.color(random_color())
@@ -20,23 +22,21 @@ def draw_spirograph(size_of_gap):
         tim.setheading(tim.heading() + size_of_gap)
 
 draw_spirograph(5)
-
 '''
-timmy_turtle = Turtle()
-timmy_turtle.shape("turtle")
-timmy_turtle.color("green")
-timmy_turtle.penup()
-timmy_turtle.setposition(-32.5,375)
-timmy_turtle.pendown()
+
+tim.shape("turtle")
+tim.color("green")
+tim.penup()
+tim.setposition(-32.5,375)
+tim.pendown()
 
 def draw_shape(num_sides):
     angle = 360/num_sides
     for _ in range(num_sides):
-        timmy_turtle.forward(75)
-        timmy_turtle.right(angle)
+        tim.forward(75)
+        tim.right(angle)
 
 for shape_side_n in range(3, 30):
     draw_shape(shape_side_n)
-'''
-screen = turtle_module.Screen()
+
 screen.exitonclick()
